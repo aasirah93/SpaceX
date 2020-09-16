d05498395652a34726a88ed8f54a2999a95d9865
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-09-16 23:18:40 +0100

    Pasted from prev app

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
 
 export default App;
+
+import React, { useState, useEffect } from 'react';
+import Starwars from './components/starwars'
+import Pagination from './components/pagination'
+import Searchbar from './components/searchbar'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  
+  const [planet, setPlanet] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://swapi.dev/api/planets/")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
