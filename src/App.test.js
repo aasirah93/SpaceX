2f853098bf9c96f5c774095c05eb681a5d03be9e
Asifur Rahman <asirah1@googlemail.com>
2020-09-29 23:39:29 +0100

    Broken fetch test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen, act } from '@testing-library/react';
+import App from './app'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({
+    id : 1,
+    rocket_name : "Falcon 1"
+  })
+}))
+
+describe("App", () => {
+  it("loads the rocket on mount", async () => {
+   await act(async () => render(<App/>));
+   expect(screen.getByText("rocket_name")).toBeInTheDocument();
+  })
+
+})
+
+
+  
+  
